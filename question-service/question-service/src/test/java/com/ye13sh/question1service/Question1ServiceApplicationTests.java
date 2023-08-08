fdf9eac6f66d636492a77d361a7fdda655fc68a5
Yeshwanthh Kumar <115858058+ye13sh@users.noreply.github.com>
2023-08-08 23:14:14 +0530

    Add files via upload

---
+++ question-service/question-service/src/test/java/com/ye13sh/question1service/Question1ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ye13sh.question1service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Question1ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
