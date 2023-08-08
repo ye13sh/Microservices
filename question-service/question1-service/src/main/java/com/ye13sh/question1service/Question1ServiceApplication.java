f5b2a13e899946e347b4be5b27a3bd97c049321a
Yeshwanthh Kumar <115858058+ye13sh@users.noreply.github.com>
2023-08-08 23:11:04 +0530

    Add files via upload

---
+++ question-service/question1-service/src/main/java/com/ye13sh/question1service/Question1ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ye13sh.question1service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question1ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Question1ServiceApplication.class, args);
+	}
+
+}
